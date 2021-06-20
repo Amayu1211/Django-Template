d8edc6ac4241333de3dd703711ed89f1a2f97820
unknown <pawarmayuri681@gmail.com>
2021-06-20 16:03:22 +0530

    first commit

---
+++ tem/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'home.html')
+def removepunc(request):
+    return HttpResponse("remove punc")
+def capfirst(request):
+    return HttpResponse("capitalize first")
+def newlineremove(request):
+    return HttpResponse("newline remove first")
+def spaceremove(request):
+    return HttpResponse("space remover back")
+def charcount(request):
+    return HttpResponse("charcount ")
